97e983317cc43f173c30bc9d8c574489e5e16b59
Chris Franklin <cf73021@gmail.com>
2019-02-07 21:27:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  }
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+// add name to the blt before serving
+blt.name = "BLT";
+blt.serve();
+function serve() {
+  console.log("here's your " + this.name + ", enjoy!");
+}
